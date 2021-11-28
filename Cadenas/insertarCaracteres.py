355edf21eac664f70b8e919ab78432bf221048e2
Oliverio Mourier <om.mourier@gmail.com>
2021-11-28 19:42:33 -0300

    ejercicio cadenas

---
+++ Cadenas/insertarCaracteres.py
@@ -0,0 +1,45 @@
+#Escribir funciones que dada una cadena y un caracter:
+#a) Inserte el caracter entre cada letra de la cadena. Ej: 'separar' y ',' debería devolver
+#'s,e,p,a,r,a,r'
+
+def insertarCaracter(palabra):
+    palabraConCaracter=""
+    
+    for i in range(len(palabra)):
+        palabraConCaracter += palabra[i]+","
+        
+    return palabraConCaracter[:len(palabraConCaracter)-1]
+
+print(insertarCaracter("perro"))
+
+
+#b) Reemplace todos los espacios por el caracter. Ej: 'mi archivo de texto.txt' y '_'
+#debería devolver 'mi_archivo_de_texto.txt'
+
+def reemplazarEspacioPorCaracter(palabra, caracter):
+    palabraSeparada = palabra.split()
+    return caracter.join(palabraSeparada)
+
+print(reemplazarEspacioPorCaracter('mi archivo de texto.txt', '_'))
+
+
+#c) Reemplace todos los dígitos en la cadena por el caracter. Ej: 'su clave es: 1540' y
+#'X' debería devolver 'su clave es: XXXX'
+
+def reemplazarDigitos(palabra):
+    palabraSinDigitos = ""
+    
+    for i in range(len(palabra)):
+        if(palabra[i].isnumeric()):
+            palabraSinDigitos += "X"
+        else:
+            palabraSinDigitos += palabra[i]
+    
+    return palabraSinDigitos
+
+print(reemplazarDigitos('su clave es: 1540'))
+    
+    
+#d) Inserte el caracter cada 3 dígitos en la cadena. Ej. '2552552550' y '.' debería devolver
+#'255.255.255.0'
+
