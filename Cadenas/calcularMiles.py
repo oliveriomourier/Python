40000bbac4a0bdb2040b996999300103ef3e8e2e
Oliverio Mourier <om.mourier@gmail.com>
2021-11-28 20:12:47 -0300

    ejercicio completo

--- Cadenas/calcularMiles.py
+++ Cadenas/calcularMiles.py
@@ -3,4 +3,11 @@
 #'1234567890' , debe devolver '1.234.567.890' .
 
 def calculcarMiles(numero):
-    
+    numeroNuevo = ""
+    for i in range(len(numero)-1, -1, -1):
+        if i%3 == 0 and i != len(numero)-1:
+            numeroNuevo += "."
+        numeroNuevo += numero[i]
+    print(numeroNuevo[::-1])
+        
+calculcarMiles('1234567890' )
