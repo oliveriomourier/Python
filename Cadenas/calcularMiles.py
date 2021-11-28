b66b900b509a14d8de62510302f0e094813e71ba
Oliverio Mourier <om.mourier@gmail.com>
2021-11-28 20:03:11 -0300

    ejercicio

---
+++ Cadenas/calcularMiles.py
@@ -0,0 +1,6 @@
+#Ejercicio 6.4. Escribir una función que reciba una cadena que contiene un largo número en-
+#tero y devuelva una cadena con el número y las separaciones de miles. Por ejemplo, si recibe
+#'1234567890' , debe devolver '1.234.567.890' .
+
+def calculcarMiles(numero):
+    
