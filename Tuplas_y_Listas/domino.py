ecda681e576b53b0e5c68449be0029038f6e399b
Oliverio Mourier <om.mourier@gmail.com>
2021-11-29 12:26:21 -0300

    ejercicio domino b)

--- Tuplas_y_Listas/domino.py
+++ Tuplas_y_Listas/domino.py
@@ -15,3 +15,22 @@ def lasFichasEncajan(tupla1, tupla2):
 print(lasFichasEncajan((1,4), (1,2)))
 print(lasFichasEncajan((1,1), (4,4)))
 
+
+#b) Escribir una función que indique si dos fichas de dominó encajan o no. Las fichas son
+#recibidas en una cadena, por ejemplo: 3-4 2-5 . Nota: utilizar la función split de las
+#cadenas.
+
+def fichasEncajan(cadena1, cadena2):
+    cadena1Separada = cadena1.split('-')
+    cadena2Separada = cadena2.split('-')
+    encajan = False
+    
+    for elemento in cadena1Separada:
+        if elemento in cadena2Separada:
+            encajan = True
+            return encajan
+    
+    return encajan
+
+print(fichasEncajan("1-4", "1-2"))
+print(fichasEncajan("1-1", "4-4"))
