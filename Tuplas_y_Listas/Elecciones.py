1c895471d1501217db86a0accef6516027b2110f
Oliverio Mourier <om.mourier@gmail.com>
2021-11-30 18:44:08 -0300

    Ejercicio domino b)

--- Tuplas_y_Listas/Elecciones.py
+++ Tuplas_y_Listas/Elecciones.py
@@ -7,11 +7,21 @@ def imprimirPedidoDeVoto(tupla):
     for nombre in tupla:
         print(f"Estimado {nombre}, vote por mi")
 
+
+#imprimirPedidoDeVoto(("Oliverio", "Juan", "Manuel"))
 #b) Escribir una función que reciba una tupla con nombres, una posición de origen p y una
 #cantidad n , e imprima el mensaje anterior para los n nombres que se encuentran a partir
 #de la posición p .
+        
+def imprimirDesdeHasta(tuplaNombres, origen, final):
+    
+    for i in range(origen, origen + final):
+        print(f"Estimado {tuplaNombres[i]}, vote por mi")
+    
+imprimirDesdeHasta(("Oliverio", "juan", "manuel", "seba", "pino", "alberto", "elOgro"), 2, 1)  
+    
+    
+    
+    
 #c) Modificar las funciones anteriores para que tengan en cuenta el género del destinatario,
 #para ello, deberán recibir una tupla de tuplas, conteniendo el nombre y el género.
-        
-    
-imprimirPedidoDeVoto(("Oliverio", "Juan", "Manuel"))
