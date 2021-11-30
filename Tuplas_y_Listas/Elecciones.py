a9fdf86bf958ccc04e881f461843d8dd323b1be8
Oliverio Mourier <om.mourier@gmail.com>
2021-11-30 18:52:15 -0300

    Ejercicio domino c)

--- Tuplas_y_Listas/Elecciones.py
+++ Tuplas_y_Listas/Elecciones.py
@@ -18,10 +18,37 @@ def imprimirDesdeHasta(tuplaNombres, origen, final):
     for i in range(origen, origen + final):
         print(f"Estimado {tuplaNombres[i]}, vote por mi")
     
-imprimirDesdeHasta(("Oliverio", "juan", "manuel", "seba", "pino", "alberto", "elOgro"), 2, 1)  
-    
-    
+#imprimirDesdeHasta(("Oliverio", "juan", "manuel", "seba", "pino", "alberto", "elOgro"), 2, 1)  
     
     
 #c) Modificar las funciones anteriores para que tengan en cuenta el género del destinatario,
 #para ello, deberán recibir una tupla de tuplas, conteniendo el nombre y el género.
+
+
+def imprimirPorGenero(tupla_de_tuplas, origen, final):
+    nombre = 0
+    genero = 1
+    for i in range(origen, origen + final):
+        if tupla_de_tuplas[i][genero] == "Masculino":
+            print(f"Estimado {tupla_de_tuplas[i][nombre]}, vote por mi")
+        else:
+            print(f"Estimada {tupla_de_tuplas[i][nombre]}, vote por mi")
+        
+        
+        
+        
+imprimirPorGenero((("Oliverio", "Masculino"), ("juan", "Masculino"), ("manuela", "Femenino"), ("Emilia", "Femenino"), ("Raquel", "Femenino"), ("alberto", "Masculino"), ("Anabela", "Femenino")), 1, 3)  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
