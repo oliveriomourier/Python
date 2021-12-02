81e17948d3a64d6e8f5a1315500d0b8f53e135b0
Oliverio Mourier <om.mourier@gmail.com>
2021-12-02 19:53:24 -0300

    ejercicio diccioanrios

---
+++ Diccionarios/devolverIniciales.py
@@ -0,0 +1,46 @@
+#Ejercicio 6.5. Escribir una función que dada una cadena de caracteres, devuelva:
+#a) La primera letra de cada palabra. Por ejemplo, si recibe 'Universal Serial Bus' debe
+#devolver 'USB' .
+
+def devolverIniciales(palabra):
+    palabraSeparada = palabra.split()
+    iniciales = ""
+    
+    for palabra in palabraSeparada:
+        iniciales += palabra[0]
+        
+    return iniciales
+
+#print(devolverIniciales('Universal Serial Bus'))
+    
+
+#b) Dicha cadena con la primera letra de cada palabra en mayúsculas. Por ejemplo, si recibe
+#'república argentina' debe devolver 'República Argentina' .
+
+def devolverInicialess(palabra):
+    palabraSeparada = palabra.split()
+    
+    for palabra in palabraSeparada:
+        print(palabra.capitalize(), end=" ")
+
+#devolverInicialess('republica argentina')
+
+
+#c) Las palabras que comiencen con la letra ‘A’. Por ejemplo, si recibe 'Antes de ayer'
+#debe devolver 'Antes ayer'
+
+def palabrasQueEmpiecenCon(oracion, letra):
+    palabraSeparada = oracion.split()
+    palabraQueEmpiezaConLetra = []
+    
+    for palabra in palabraSeparada:
+        if palabra[0].upper() == letra.upper():
+            palabraQueEmpiezaConLetra.append(palabra)
+            
+    for palabra in palabraQueEmpiezaConLetra:
+        print(palabra, end = " ")
+        
+palabrasQueEmpiecenCon('Antes de ayer', 'A')
+        
+    
+            
